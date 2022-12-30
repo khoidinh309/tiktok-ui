73184a728cbc5b5adf35b9877007aa5a4ebea9a1
KhoiDinh <khoi.dinhiamkhoi@hcmut.edu.vn>
2022-12-30 14:29:28 +0700

    update Header UI #2

---
+++ src/Components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/05aa222706c452c8ad0ea7d2e719a745~c5_100x100.jpeg?x-expires=1672552800&x-signature=tDVwpte%2FS8qLcXACzDnJ66SmbjA%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Văn A</span>
+                    <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('userName')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
